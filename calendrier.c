86b13e02c1321757f003200da96a4e69a87378e1
Lamine Diallo <bah07565@gmail.com>
2022-07-15 21:33:28 +0100

    nouvelle affichage du clalendrier

--- calendrier.c
+++ calendrier.c
@@ -16,11 +16,12 @@ int main(int argc, char const *argv[])
 
     do
     {
+        system("color f0");
         int annee, mois;
 
-        printf("\n");
         do
         {
+        printf("\n");
             printf("!!! Une annee comporte 12 mois pour info !!!\n");
             printf(" Saisir le mois --> ");
             scanf("%d", &mois);
@@ -45,14 +46,31 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-/* --------------------------------------------------------- */
+/**
+ * > La fonction prend une date en entrée et renvoie le jour de la semaine du premier jour du mois de
+ * la date en entrée
+ *
+ * @param jour Le jour du mois (1-31)
+ * @param mois Le mois pour lequel vous souhaitez obtenir le jour de la semaine.
+ * @param annee an
+ *
+ * @return Le jour premiere de la semaine.
+ */
 int jour_du_debut_de_mois(int jour, int mois, int annee)
 {
-    static int t[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
+    int t[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
     annee -= mois < 3;
     return (annee + (annee / 4) - (annee / 100) + (annee / 400) + t[mois - 1] + jour) % 7;
 }
-/* --------------------------------------------------------- */
+
+/**
+ * Elle prend un entier comme argument et renvoie une chaîne contenant le nom du mois correspondant à
+ * l'entier
+ *
+ * @param nbr_du_mois Le numéro du mois.
+ *
+ * @return Le nom du mois.
+ */
 char *nom_du_mois(int nbr_du_mois)
 {
     char *mois = NULL;
@@ -98,7 +116,15 @@ char *nom_du_mois(int nbr_du_mois)
     }
     return mois;
 }
-/* --------------------------------------------------------- */
+
+/**
+ * Il renvoie le nombre de jours dans un mois, étant donné le numéro du mois et l'année
+ *
+ * @param nbr_du_mois Le numéro du mois (1-12)
+ * @param annee an
+ *
+ * @return Le nombre de jours dans un mois.
+ */
 int nbr_de_jour(int nbr_du_mois, int annee)
 {
     if (nbr_du_mois == 1)
@@ -141,31 +167,40 @@ int nbr_de_jour(int nbr_du_mois, int annee)
     else if (nbr_du_mois == 12)
         return (31);
 }
-/* --------------------------------------------------------- */
+
+/**
+ * Il imprime le calendrier pour l'année et le mois donnés
+ *
+ * @param annee l'année
+ * @param mois mois
+ *
+ * @return le nombre de jours dans le mois.
+ */
 int affichage_du_calendrier(int annee, int mois)
 {
     int jour, cpt, i, j, k;
 
     jour = nbr_de_jour(mois, annee);
+    
+    printf("\n\t   ------------------ ------------------ ------------------ \n\n");
+    printf("\t \t \t        %s  %d                 \n\n", nom_du_mois(mois), annee);
+    printf("\t   ------------------ ------------------ ------------------ \n\n");
 
-    printf("  ---------------------------");
-    printf("\n    \t %s   %d\n", nom_du_mois(mois), annee);
-    printf("  ---------------------------\n");
-    printf("   L   M   M   J   V   S   D\n");
+    printf("\t    Lun\t    Mar\t    Mer\t    Jeu\t    Ven\t    Sam\t    Dim \n\n\n");
 
     cpt = jour_du_debut_de_mois(1, mois, annee);
     --cpt;
 
     for (k = 0; k < cpt; k++)
-        printf("    ");
+        printf("\t       ");
 
     for (j = 1; j <= jour; j++)
     {
-        printf("%4d", j);
+        printf("\t  %5d", j);
         if (++k > 6)
         {
             k = 0;
-            printf("\n");
+            printf("\n\n\n");
         }
     }
 
